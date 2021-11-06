b4587807fb1e87c2b5a618c409d230cbbddb583e
maarioos123 <t8200003@aueb.gr>
2021-11-06 14:56:07 +0200

    adding Handler class that creates,removes,ticks and renders all the objects of the game

---
+++ src/Handler.java
@@ -0,0 +1,32 @@
+import java.awt.Graphics;
+import java.util.LinkedList;
+
+public class Handler {
+    //creating a linkedlist that adds and removes Game Objects dynamically
+    LinkedList<GameObject> object = new LinkedList<GameObject>();
+
+    //loops through all Game Objects from list and ticks them
+    public void tick() {
+        for (int i = 0; i < object.size(); i++) {
+            GameObject temp = object.get(i);
+            temp.tick();
+        }
+    }
+
+    //loops through all objects from the list and renders them
+    public void render (Graphics g){
+        for (int i = 0; i < object.size(); i++) {
+            GameObject temp = object.get(i);
+            temp.render(g);
+        }
+    }
+
+    //adds new object to the list
+    public void addObject (GameObject object){
+        this.object.add(object);
+    }
+    //removes objects from list
+    public void removeObject (GameObject object){
+        this.object.remove(object);
+    }
+}
