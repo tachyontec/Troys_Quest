4d8cc6fbd35961920b288cf8348c91813add534b
Mike-Gag <t8200234@aueb.gr>
2021-11-10 00:51:32 +0200

    Remade the menu via a new method methods LaunchPage and NewWindow deleted , not needed anymore

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+public class Game {
+
+    private final int WIDTH = 900;
+    private final int HEIGHT = 650;
+
+    public Game() {
+        GameWindow window = new GameWindow(WIDTH,HEIGHT,"Troy's Quest");
+    }
+
+    public static void main(String[] args)
+    {
+        new Game();
+    }
+}
