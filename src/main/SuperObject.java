18a11ab88c2266160aa4f398136d2e756694b0d9
TeoSkondras <290901As>
2021-11-17 18:09:51 +0200

    Adding SuperObject.java, the superclass of our objects (not players)

---
+++ src/main/SuperObject.java
@@ -0,0 +1,56 @@
+package main;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class SuperObject {
+
+    private BufferedImage image;
+    private String name;
+    private boolean collision = false;
+    private int x,y;
+
+    public void draw(Graphics2D graphics2D, GamePanel gamePanel) {
+
+        graphics2D.drawImage(image , x ,y , gamePanel.tileSize , gamePanel.tileSize , null);
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCollision() {
+        return collision;
+    }
+
+    public void setCollision(boolean collision) {
+        this.collision = collision;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
