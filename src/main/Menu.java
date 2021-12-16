b14fe403b5fe1ea02245db8554517d530c7c4256
NickKatsios <t8200071@aueb.gr>
2021-12-16 19:02:10 +0200

    Make win_lose menu match main menu with a twist Cut code in win_lose menu drawing in half

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -15,6 +15,7 @@ public class Menu {
     BufferedImage backgroundImage;
     BufferedImage buttonImage;
     BufferedImage tombImage;
+    BufferedImage wreath;
 
     public int choice = 0; //option selection default
     int defaultX = 200;
@@ -29,6 +30,7 @@ public class Menu {
             this.backgroundImage = ImageIO.read(new File("res/Backgrounds/backgroundMenu.png"));
             this.buttonImage = ImageIO.read(new File("res/Buttons/Button.png"));
             this.tombImage = ImageIO.read(new File("res/menu/tombstone2.png"));
+            this.wreath =  ImageIO.read(new File("res/menu/olive wreath.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -81,7 +83,7 @@ public class Menu {
         g2.drawImage(buttonImage , defaultX - 50 , defaultY - 50 , gamepanel.tileSize * 9 , 60  ,null);
         g2.setFont(g2.getFont().deriveFont(Font.BOLD, 80));
         //Title
-        String title = "Pause";
+        String title = "PAUSE";
         g2.setColor(Color.BLACK);
         g2.drawString(title, 233, 103);
         g2.setColor(new Color(181, 179, 92));
@@ -112,57 +114,47 @@ public class Menu {
         //if he has lives left we draw the win menu
         //to be optimized for less lines of code
         g2.setFont(menuFont);
-        if (gamepanel.player.getLivesLeft() > 0) {
-            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 80));
-            g2.setColor(new Color(200, 179, 92));
-            String title = "YOU WIN!";
-            g2.drawString(title, gamepanel.screenWidth / 2 - 180, gamepanel.screenHeight / 2 - 200);
-            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 40));
-            g2.drawString("YOUR TIME WAS: " + decFormat.format(gamepanel.hud.levelTimer) + "sec", gamepanel.screenWidth / 2 - 240, 150);
-            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 50));
-            String option;
-            option = "REPLAY LEVEL";
-            g2.drawString(option, 180, 260);
-            if (choice == 0) {
-                g2.drawString(">", 150, 260);
-            }
-            option = "BACK TO MAIN MENU";
-            g2.drawString(option, 180, 320);
-            if (choice == 1) {
-                g2.drawString(">", 150, 320);
-            }
-            option = "EXIT";
-            g2.drawString(option, 180, 380);
-            if (choice == 2) {
-                g2.drawString(">", 150, 380);
-            }
-        } else { //if he has not any lives left we draw the lose menu
             //BACKGROUND
-            g2.drawImage(tombImage , 0 , 0 , gamepanel.tileSize * 6 , gamepanel.tileSize * 5 , null);
+            g2.drawImage(backgroundImage , 0  , 0 ,gamepanel.tileSize * 16 , gamepanel.tileSize * 12  , null );
+            g2.drawImage(buttonImage , defaultX - 50 , defaultY + 70 , gamepanel.tileSize * 9 , 60  ,null);
+            g2.drawImage(buttonImage , defaultX - 50 , defaultY + 10, gamepanel.tileSize * 9 , 60  ,null);
+            g2.drawImage(buttonImage , defaultX - 50 , defaultY - 50 , gamepanel.tileSize * 9 , 60  ,null);
+            String title;
+            if(gamepanel.player.getLivesLeft() == 0){
+                g2.drawImage(tombImage , 250 , 110 , gamepanel.tileSize * 5 , gamepanel.tileSize * 3 , null);
+                title = "YOU LOSE!";
+            } else {
+                title = "YOU WIN!";
+                g2.drawImage(wreath , 250 , 107 , gamepanel.tileSize * 5 , gamepanel.tileSize * 3 , null);
+
+            }
+            //Title
             g2.setFont(g2.getFont().deriveFont(Font.BOLD, 80));
-            g2.setColor(new Color(200, 179, 92));
-            String title = "YOU LOSE!";
-            g2.drawString(title, gamepanel.screenWidth / 2 - 180, gamepanel.screenHeight / 2 - 200);
-            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 40));
-            //g2.drawString("YOUR TIME WAS: " + decFormat.format(gamepanel.hud.deathTime) + "sec", gamepanel.screenWidth / 2 - 240, 150);
-            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 50));
+            g2.setColor(Color.BLACK);
+            g2.drawString(title, 153, 103);
+            g2.setColor(new Color(181, 179, 92));
+            g2.drawString(title, 150, 100);
+            //Options
+            g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 40));
             String option;
             option = "REPLAY LEVEL";
-            g2.drawString(option, 180, 260);
+            g2.drawString(option, defaultX, defaultY);
             if (choice == 0) {
-                g2.drawString(">", 150, 260);
+                g2.drawString(">", defaultX - 30, defaultY);
             }
             option = "BACK TO MAIN MENU";
-            g2.drawString(option, 180, 320);
+            g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 33));
+            g2.drawString(option, defaultX - 5, defaultY + 57);
+            g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 40));
             if (choice == 1) {
-                g2.drawString(">", 150, 320);
+                g2.drawString(">", defaultX - 30, defaultY + 60);
             }
             option = "EXIT";
-            g2.drawString(option, 180, 380);
+            g2.drawString(option, defaultX + 50, defaultY + 120);
             if (choice == 2) {
-                g2.drawString(">", 150, 380);
+                g2.drawString(">", defaultX + 20, defaultY + 120);
             }
-        }
+
     }
 
 
